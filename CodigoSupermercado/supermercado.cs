7abff58d271825dd28e04782d7d608ba316e24ed
abea2007 <166263554+abea2007@users.noreply.github.com>
2024-08-26 10:45:18 -0600

    Create supermercado.cs

---
+++ CodigoSupermercado/supermercado.cs
@@ -0,0 +1,30 @@
+public class Program
+{
+    public static void Main(string[] args, double)
+    {
+        string producto;
+        int codigo;
+        double precioproduct;
+        bool disponibilidad;
+        
+        double descuento;
+
+        producto = "leche";
+        codigo = 123;
+        precioproduct = 200.00;
+        
+
+
+        Console.WriteLine("Bienvendios a nuestro Supermarket");
+        Console.WriteLine("Nombre del producto: ", producto);
+        Console.WriteLine("Codigo del producto: ", codigo); 
+        
+        Console.WriteLine("Ingrese el precio del producto: ");
+        
+        Console.WriteLine("Ingrese la categoria del producto: ");
+        string categoria = Console.ReadLine();
+
+        if (!(categoria = !(!"A" || precioproduct <= 100.00)))
+        {
+            
+        }
